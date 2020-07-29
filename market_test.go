71e72abf825ec8711d252478b5e4505f7f993db4
wamuir <17497234+wamuir@users.noreply.github.com>
2020-07-29 16:36:52 -0700

    simplify mmtype tests

--- market_test.go
+++ market_test.go
@@ -12,30 +12,15 @@ func sts(s string) *bufio.Scanner {
 	return bufio.NewScanner(r)
 }
 
-func TestMMTypeIsMMType(t *testing.T) {
-
-	for i := range supported {
-		for j := range supported {
-			if i == j {
-				continue
-			}
-			if supported[i].isMMType(&supported[j]) != (i == j) {
-				t.Errorf(
-					"Expected equality of types to evaluate %v, received: %v",
-					(i == j),
-					!(i == j),
-				)
-			}
-
-		}
-	}
-
-}
-
 func TestScanHeader(t *testing.T) {
 
-	// valid header
+	// example valid coordinate header
 	if _, err := scanHeader(sts(`%%MatrixMarket matrix coordinate integer general`)); err != nil {
+		t.Errorf(err.Error())
+	}
+
+	// example valid array header
+	if _, err := scanHeader(sts(`%%MatrixMarket matrix array complex hermitian`)); err != nil {
 		t.Errorf(err.Error())
 	}
 
@@ -44,14 +29,24 @@ func TestScanHeader(t *testing.T) {
 		t.Errorf("Expected EOF error, received: %v", err)
 	}
 
-	// object field missing from header
+	// too few fields in header
 	if _, err := scanHeader(sts(`%%MatrixMarket coordinate integer general`)); err == nil {
 		t.Errorf("Expected EOF error, received: %v", err)
+	}
+
+	// superfluous field(s) in header (expect to be discarded)
+	if _, err := scanHeader(sts(`%%MatrixMarket matrix coordinate integer general extra`)); err != nil {
+		t.Errorf("Expected nil error, received: %v", err)
 	}
 
 	// malformed banner
 	if _, err := scanHeader(sts(`MatrixMarket matrix coordinate integer general`)); err != ErrNoHeader {
 		t.Errorf("Expected NO_HEADER error, received: %v", err)
+	}
+
+	// unsupported object field
+	if _, err := scanHeader(sts(`%%MatrixMarket xirtam coordinate integer general`)); err == nil {
+		t.Errorf("Expected EOF error, received: %v", err)
 	}
 
 	// invalid field combination (real and hermitian)
@@ -64,8 +59,4 @@ func TestScanHeader(t *testing.T) {
 		t.Errorf("Expected UNSUPPORTED_TYPE error, received: %v", err)
 	}
 
-	// superfluous field(s) in header (expect to be discarded)
-	if _, err := scanHeader(sts(`%%MatrixMarket matrix coordinate integer general extra`)); err != nil {
-		t.Errorf("Expected nil error, received: %v", err)
-	}
 }
