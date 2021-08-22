83dbd4213f2b652e0651b80ca9507e8a659f3088
William Muir <17497234+wamuir@users.noreply.github.com>
2021-08-21 20:38:45 -0500

    reformat package documentation

--- doc.go
+++ doc.go
@@ -2,8 +2,8 @@
 // The use of this source code is governed by the MIT License that can be found
 // in the LICENSE file.
 
-// Package matrix-market reads and writes in the NIST Matrix Market native
-// exchange format.  The Matrix Market is a service of the Mathematical and
+// Package market reads and writes in the NIST Matrix Market native exchange
+// format.  The Matrix Market is a service of the Mathematical and
 // Computational Sciences Division of the Information Technology Laboratory
 // of the National Institute of Standards and Technology (NIST), containing
 // "test data for use in comparative studies of algorithms for numerical
@@ -11,5 +11,4 @@
 // of applications, as well as matrix generation tools and services." The
 // Matrix Market native exchange format has become a standard for exchanging
 // matrix data.
-
 package market
